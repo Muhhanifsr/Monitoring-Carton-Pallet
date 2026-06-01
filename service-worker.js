835361cf4c78db592b42c2e0edf4407486f4d819
Muhhanifsr <muhammadhanifsaveroriyadi@gmail.com>
2026-06-01 09:36:38 +0700

    update cache name

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
  *  - Google Apps Script API calls                 → Network First (no cache)
  */
 
-const CACHE_NAME = 'stockmonitor-v1.9';
+const CACHE_NAME = 'stockmonitor-v2.0';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const STATIC_ASSETS = [
   '',
